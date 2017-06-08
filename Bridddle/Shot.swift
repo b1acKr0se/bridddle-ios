7fec29ceca645e61947917c6cb58a4ad705459ae
b1acKr0se <nguyen.thanh.hai@framgia.com>
2017-06-08 10:58:29 +0700

    Implement UICollectionView

---
+++ Bridddle/Shot.swift
@@ -0,0 +1,18 @@
+//
+//  Shot.swift
+//  Bridddle
+//
+//  Created by nguyen.thanh.hai on 6/8/17.
+//  Copyright © 2017 Hai Nguyen. All rights reserved.
+//
+
+import Foundation
+
+class Shot {
+    var url: String
+    
+    init(url: String) {
+        self.url = url
+    }
+}
+
